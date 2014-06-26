ec5f30836d0825eb4a449961523bcae944c13923
Alexis Seigneurin <alexis@seigneurin.com>
2014-06-26 17:24:04 +0200

    Allow uploading files from streams

--- Aspose.Cloud.SDK/src/com/aspose/cloud/common/Utils.java
+++ Aspose.Cloud.SDK/src/com/aspose/cloud/common/Utils.java
@@ -5,9 +5,6 @@ package com.aspose.cloud.common;
 
 import groovyx.net.http.URIBuilder;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -152,11 +149,11 @@ public class Utils {
 
 	}
 
-	public static String UploadFileBinary(File localFile, String uploadUrl,
+	public static String UploadFileBinary(InputStream inputStream, String uploadUrl,
 			String strHttpCommand) {
 		try {
 			URL url = new URL(uploadUrl);
-			byte[] buf = getBytesFromFile(localFile);
+			byte[] buf = IOUtils.toByteArray(inputStream);
 			HttpURLConnection m_connection;
 			m_connection = (HttpURLConnection) url.openConnection();
 			//String parameters = "data=some_post_data";
@@ -182,42 +179,6 @@ public class Utils {
 			return null;
 		}
 
-	}
-
-	// Returns the contents of the file in a byte array.
-	public static byte[] getBytesFromFile(File file) throws IOException {
-		InputStream is = new FileInputStream(file);
-
-		// Get the size of the file
-		long length = file.length();
-
-		// You cannot create an array using a long type.
-		// It needs to be an int type.
-		// Before converting to an int type, check
-		// to ensure that file is not larger than Integer.MAX_VALUE.
-		if (length > Integer.MAX_VALUE) {
-			// File is too large
-		}
-
-		// Create the byte array to hold the data
-		byte[] bytes = new byte[(int) length];
-
-		// Read in the bytes
-		int offset = 0;
-		int numRead = 0;
-		while (offset < bytes.length
-				&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-			offset += numRead;
-		}
-
-		// Ensure all the bytes have been read in
-		if (offset < bytes.length) {
-			throw new IOException("Could not completely read file "	+ file.getName());
-		}
-
-		// Close the input stream and return bytes
-		is.close();
-		return bytes;
 	}
 
 	public static InputStream ProcessCommand(String strURI,
